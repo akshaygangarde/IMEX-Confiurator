6f4adc5bc2955506c20a4acadaa6edffc918cb67
akshaygangarde <akshaygangarde@gmail.com>
2020-12-26 09:10:30 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  componentDidMount(){
+    console.log(document.body.getElementsByClassName('col'))
+    var col = document.body.getElementsByClassName('col');
+    console.log("childnodes", col[0].children)
+    col[0].removeChild(col[0].children[0]);
+    console.log("childnodes", col[0].children)
+  }
+
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
